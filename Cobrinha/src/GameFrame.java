5941ece53aa649402202e50ae9087c6738d01b73
Bill Veiga <gabrielbillveiga@gmail.com>
2021-07-14 11:51:49 -0300

    add lines to GameFrame.Java and GamePAnel.Java
    some code add to the blank templates

--- Cobrinha/src/GameFrame.java
+++ Cobrinha/src/GameFrame.java
@@ -2,6 +2,13 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
 	GameFrame(){
+		this.add(new GamePanel());
+		this.setTitle("Cobrinha");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
 		
 	}
 }
--- Cobrinha/src/GamePanel.java
+++ Cobrinha/src/GamePanel.java
@@ -1,11 +1,49 @@
-import java.awt.event.ActionEvent;
-
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.event.*;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyAdapter;
+import javax.swing.*;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener{
+	
 	GamePanel(){
 		
 	}
+	public void startGame() {
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+	}
+	public void draw(Graphics g) {
+		
+	}
+	public void move() {
+		
+	}
+	public void checkApple() {
+		
+	}
+	public void checkCollisions() {
+		
+	}
+	public void gameOver(Graphics g) {
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	public class MyKeyAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+	}
+	
 }
+
+
+
+
