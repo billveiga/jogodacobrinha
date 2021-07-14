082951f58bc09ee29ec977097efd4be864fcd26a
Bill Veiga <gabrielbillveiga@gmail.com>
2021-07-13 22:54:04 -0300

    Add files via upload

---
+++ Cobrinha/src/GamePanel.java
@@ -0,0 +1,11 @@
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+	GamePanel(){
+		
+	}
+}
