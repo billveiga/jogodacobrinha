8cf6135f0903575f53a7eb50e7eaf1d90ef5d793
Bill Veiga <gabrielbillveiga@gmail.com>
2021-07-14 12:35:12 -0300

    add apple

--- Cobrinha/src/GamePanel.java
+++ Cobrinha/src/GamePanel.java
@@ -48,10 +48,17 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void draw(Graphics g) {
-		
+		for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
+			g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+		}
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 	}
+	
 	public void newApple() {
-		
+		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+		appleX = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 	}
 	public void move() {
 		
