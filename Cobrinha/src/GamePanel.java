ff748727f9fcadd4b21612fe90d6acd9da3264d5
Bill Veiga <gabrielbillveiga@gmail.com>
2021-07-15 10:56:54 -0300

    Updade GamePanel.java
    add switch case for control the game

--- Cobrinha/src/GamePanel.java
+++ Cobrinha/src/GamePanel.java
@@ -61,7 +61,25 @@ public class GamePanel extends JPanel implements ActionListener{
 		appleX = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
 	}
 	public void move() {
-		
+		for(int i = bodyParts;i>0;i--){
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		switch(direction){
+			case 'U':
+				y[0] = y[0] - UNIT_SIZE;
+				break;
+			case 'D':
+				y[0] = y[0] + UNIT_SIZE;
+				break;	
+			case 'L':
+				x[0] = x[0] - UNIT_SIZE;
+				break;
+			case 'R':
+				x[0] = x[0] + UNIT_SIZE;
+				break;	
+		}
+			
 	}
 	public void checkApple() {
 		
